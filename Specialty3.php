c8a25cee1ddb7d58dc11a4c0764b3a695f847588
SoullessStoryteller <75305580+SoullessStoryteller@users.noreply.github.com>
2021-05-31 18:19:24 +0300

    Add files via upload

---
+++ Specialty3.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Мехатроника и мобильная работотехника</title>
+</head>
+
+<body>
+<!-- Шапка -->
+<div class="main-block" style="background-image: url(images/Group%20836.png)">
+<nav class="navbar navbar-expand-lg">
+  <div class="container-fluid">
+    <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="Index.php">Специальности</a>
+        </li>        
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="Index.php">Партнёры</a>
+        </li>        
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="AboutCollege.php">О колледже</a>
+        </li>        
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="Abiturients.php">Абитуриентам</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<!-- Шапка - конец -->
+    
+<!-- Блок с фоном -->
+<div style="margin: 0px 160px">
+            <div class="row">
+                <div class="col justify-content-center">
+                    <p style="color: white; font-size: 54px">Специальность</p>
+                    <p style="color: white; font-size: 40px; padding-bottom: 100px">15.02.10 <br>Мехатроника и мобильная работотехника</p>
+                    <input class="submit-button" type="button" value="Отправить заявку"/>
+                </div>
+            </div>
+        </div>    
+    </div>
+<!-- Блок с фоном - конец -->
+    <div class="container-fluid " style="overflow-x: hidden">
+        <div class="block_info text-center">
+            <h2 class="text-center" style="color: white; margin-bottom: 80px;">Обязательный уровень базовый</h2>
+            <div class="mx-auto" style="max-width: 1200px; border: 1px solid #0078B8;
+border-radius: 7px; min-height: 331.81px; background-size: cover; background-image: url(images/Group%20820%205.svg)">
+                <p class="mt-5" style="margin-bottom: 70px; font-size: 32px; color: white">Информация о квалификации <br> специальности</p>
+                <input class="submit-button" type="button" value="Подробнее" style="border: 2px solid #0078B8;"/>
+
+            </div>
+            <div class="send" style="margin-bottom: 100px;">
+                <h2 style="color: white; font-size: 58px; margin-bottom: 80px; margin-top: 120px">Подать заявку</h2>
+                <form action="">
+                    <input class="send_field" placeholder="ФИО" type="text">
+                    <input class="send_field" placeholder="Телефон" type="text">
+                    <input class="send_field" placeholder="E-MAIL" type="text">
+                    <input class="send_field" placeholder="09.02.16 Мехатроника и мобильная работотехника" type="text">
+                    <input class="send_field" placeholder="В каком классе Вы обучаетесь?" type="text"><br>
+                    <input class="submit-button" type="Submit" class="mt-5" value="Отправить" style="border: 2px solid #0078B8;"/>
+                </form>
+            </div>
+        </div>
+    </div>
+<!-- Подвал -->
+    <footer>
+        <div class="container">
+            <div class="row" style="color: white">
+                <div class="col">
+                    <p style="font-weight: 700; font-size: 18px;">О КОЛЛЕДЖЕ</p>
+                    <ul>
+                        <li>О НАС</li>
+                        <li>ОСНОВНЫЕ СВЕДЕНИЯ</li>
+                        <li>ЛИЦЕНЗИЯ</li>
+                        <li>РЕКВИЗИТЫ</li>
+                    </ul>
+                </div>
+                <div class="col">
+                    <p style="font-weight: 700; font-size: 18px;">ОБРАЗОВАНИЕ</p>
+                    <ul>
+                        <li>ОТДЕЛЕНИЕ IT-КОЛЛЕДЖ</li>
+                        <li>ПЕРЕЧЕНЬ ПРОФЕССИЙ</li>
+                        <li>ЗАЯВКА НА ОБУЧЕНИЕ</li>
+                        <li>ПЕРЕЧЕНЬ ПРОФЕССИЙ ПРИЁМА 2022г.</li>
+                    </ul>
+                </div>
+                <div class="col">
+                    <p style="font-weight: 700; font-size: 18px;">КОНТАКТЫ</p>
+                    <ul>
+                        <li>ПРИЁМНАЯ КОМИССИЯ</li>
+                        <li>УЧЕБНЫЙ КОРПУС №2</li>
+                        <li>ПЛАТНЫЕ ОБРАЗОВАТЕЛЬНЫЕ УСЛУГИ</li>
+                        <li>ПРЕЧЕНЬ ПРОФЕССИЙ ПРИЁМА 2022г.</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+<!-- Подвал - конец -->    
+</body>
+</html>
