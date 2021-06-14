ab3f75836307f5e6ac25a32e67a668f654436b34
SoullessStoryteller <75305580+SoullessStoryteller@users.noreply.github.com>
2021-06-15 01:35:51 +0300

    Delete php directory

--- php/connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-$connect = mysqli_connect('localhost','root','','it-college');
-if(!$connect){
-    die("Database connection error");
-}
-$charset = 'utf8';
-mysqli_set_charset($connect, $charset);
-session_start();
-?>
